73cbcf61832f45737829a225cd3219ae58b3720f
emstarker <moinul2k7@gmail.com>
2025-01-16 02:09:31 +0600

    Update setupMiddlewares.js

--- src/middlewares/setupMiddlewares.js
+++ src/middlewares/setupMiddlewares.js
@@ -4,17 +4,23 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 module.exports = (app) => {
+  const allowedOrigins = [
+    'https://explora-core.vercel.app/', 
+    'http://localhost:3000',
+  ];
+
   app.use(helmet());
   app.use(cookieParser());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+
   app.use(
     cors({
       origin: (origin, callback) => {
         if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
+          callback(null, true); // Allow request
         } else {
-          callback(new Error('Not allowed by CORS'));
+          callback(new Error('Not allowed by CORS')); // Reject request
         }
       },
       credentials: true, // Allow cookies, headers, etc.
