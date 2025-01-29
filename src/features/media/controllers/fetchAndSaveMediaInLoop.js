a8b3709b409d689d201e3c71fdbbcddd53fd0edd
emstarker <moinul2k7@gmail.com>
2025-01-30 01:37:59 +0600

    Update fetchAndSaveMediaInLoop.js

--- src/features/media/controllers/fetchAndSaveMediaInLoop.js
+++ src/features/media/controllers/fetchAndSaveMediaInLoop.js
@@ -9,7 +9,7 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 // Controller function to fetch and save media data in a loop
 const fetchAndSaveMediaInLoop = async (req, res, next) => {
-  const { mediaType, startId, endId, delayMs = 1000 } = req.body;
+  const { mediaType, startId, endId, delayMs = 100 } = req.body;
 
   if (!mediaType || !startId || !endId || startId > endId) {
     return errorResponse(res, 'Invalid input. Ensure mediaType, startId, and endId are valid.', 400);
@@ -21,7 +21,7 @@ const fetchAndSaveMediaInLoop = async (req, res, next) => {
         // Check if media already exists in the database
         const existingMedia = await Media.findOne({ tmdb_id: id });
         if (existingMedia) {
-          console.log(`Media with ID ${id} already exists in the database. Skipping.`);
+          console.log(`-----------------------------------------<  SKIPPING  >----- ${mediaType} with ID ${id} already exists in the database.`);
           continue;
         }
 
@@ -48,17 +48,16 @@ const fetchAndSaveMediaInLoop = async (req, res, next) => {
 
         // Save data to the database
         await saveDataToDB(Media, { ...data, data_status: 'Complete' });
-        console.log(`Media with ID ${id} fetched and saved successfully.`);
-
+        console.log(`-----<  SUCCESS  >------------------------------------------ ${mediaType} with ID ${id} fetched and saved successfully.`);
+                     
         // Delay between requests
         await delay(delayMs);
       } catch (error) {
-        console.error(`Error processing media ID ${id}:`, error.message);
+        console.error(`--------------------<  NOT FOUND  >----------------------- ${mediaType} with ID ${id}:`, error.message);
         // Continue with the next iteration even if one fails
         continue;
       }
     }
-
     return successResponse(res, `Media data fetched and saved from ID ${startId} to ${endId}.`);
   } catch (error) {
     console.error('Error during loop execution:', error.message);
