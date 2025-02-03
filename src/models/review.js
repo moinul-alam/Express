69dbe30a5e22c11957ccb7165d18c3ad81713104
emstarker <emstarker@gmail.com>
2025-02-03 14:52:56 +0600

    Update review.js

--- src/models/review.js
+++ src/models/review.js
@@ -5,7 +5,7 @@ const reviewSchema = new mongoose.Schema(
     media_type: { type: String, enum: ["movie", "tv"], required: true },
     tmdb_id: { type: Number, required: true, index: true }, 
     mediaId: { type: mongoose.Schema.Types.ObjectId, ref: "Media", required: true },
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     rating: { type: Number, required: true, min: 0, max: 10 },
     comment: { type: String, default: "" },
   },
