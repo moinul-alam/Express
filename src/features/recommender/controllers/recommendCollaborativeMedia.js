69997514476f9c331077dcf4c45961a68a3276ba
emstarker <moinul2k7@gmail.com>
2025-02-14 03:22:50 +0600

    back

---
+++ src/features/recommender/controllers/recommendCollaborativeMedia.js
@@ -0,0 +1,46 @@
+const api = require('@src/utils/api'); 
+const apiCore = require('@src/utils/apiCore');
+const fetchMediaDetailsService = require('@src/features/recommender/services/fetchMediaDetailsService');
+const { successResponse, errorResponse } = require('@src/utils/responseFormatter');
+
+const recommendSimilarMedia = async (req, res, next) => {
+  const { mediaType, ratings } = req.params;
+
+  try {
+    console.log('Fetching media details for collaborative filtering: ', ratings);
+
+    try {
+      const recommenderResponse = await apiCore.post('/collaborative/v2/recommendations', ratings);
+
+      if (!recommenderResponse || recommenderResponse.status !== 200) {
+        throw new Error('Failed to fetch recommendations from the service');
+      }
+
+      const similarMediaList = recommenderResponse.data.similarMedia;
+
+      // Fetch details of each similar media item
+      const mediaDetailsPromises = similarMediaList.map(async (similarMedia) => {
+        try {
+          const tmdbId = similarMedia.tmdb_id;
+          const mediaDetails = await api.get(`/${mediaType}/${tmdbId}`);
+          return mediaDetails.data;
+        } catch (error) {
+          console.error(`Failed to fetch details for TMDB ID ${tmdbId}:`, error.message);
+          return { tmdbId, error: 'Failed to fetch media details' };
+        }
+      });
+
+      const mediaDetailsArray = await Promise.all(mediaDetailsPromises);
+
+      return successResponse(res, 'Similar media fetched successfully', mediaDetailsArray);
+    } catch (recommenderError) {
+      console.error('Error connecting to the recommender:', recommenderError.message);
+      return errorResponse(res, 'Error connecting to the recommendation service', 503);
+    }
+  } catch (error) {
+    console.error('Error fetching media details or processing recommendations:', error.message);
+    return errorResponse(res, error.message || 'Internal Server Error', 500);
+  }
+};
+
+module.exports = recommendSimilarMedia;
--- src/features/recommender/routes/recommenderRoutes.js
+++ src/features/recommender/routes/recommenderRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
+const recommendCollaborativeMedia = require('@src/features/recommender/controllers/recommendCollaborativeMedia');
 const recommendSimilarMedia = require('@src/features/recommender/controllers/recommendSimilarMedia');
 const discoverSimilarMedia = require('@src/features/recommender/controllers/discoverSimilarMedia');
 
 const router = express.Router();
 
+router.post('/collaborative/recommendations', recommendCollaborativeMedia);
 router.get('/:mediaType/:mediaId/similar', recommendSimilarMedia);
 router.post('/discover', discoverSimilarMedia);
 
