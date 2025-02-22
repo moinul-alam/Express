0169e9be0db1aa0d9e932365a66c5571036b3c1d
emstarker <moinul2k7@gmail.com>
2025-02-22 20:53:55 +0600

    db update

--- src/configs/database.js
+++ src/configs/database.js
@@ -3,8 +3,8 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-if (!process.env.MONGODB_URI) {
-    console.error('MongoDB URI is not defined in environment variables.');
+if (!process.env.MONGODB_URI_LOCAL && !process.env.MONGODB_URI_CLOUD) {
+    console.error('MongoDB URIs are not defined in environment variables.');
     process.exit(1);
 }
 
@@ -17,14 +17,28 @@ const logError = (error) => {
 };
 
 const connectDB = async () => {
+    const options = {
+        autoIndex: false,
+    };
+
+    // Try connecting to LOCAL first
     try {
-        const options = {
-            autoIndex: false,
-        };
-        await mongoose.connect(process.env.MONGODB_URI, options);
-        logMessage('MongoDB connected successfully.');
-    } catch (error) {
-        logError(error);
+        logMessage('Attempting to connect to LOCAL MongoDB...');
+        await mongoose.connect(process.env.MONGODB_URI_LOCAL, options);
+        logMessage('MongoDB connected successfully to LOCAL instance.');
+        return; // Exit the function if connection is successful
+    } catch (localError) {
+        logError(localError);
+        logMessage('Failed to connect to LOCAL MongoDB. Trying CLOUD...');
+    }
+
+    // If LOCAL connection fails, try connecting to CLOUD
+    try {
+        await mongoose.connect(process.env.MONGODB_URI_CLOUD, options);
+        logMessage('MongoDB connected successfully to CLOUD instance.');
+    } catch (cloudError) {
+        logError(cloudError);
+        console.error('Failed to connect to both LOCAL and CLOUD MongoDB instances.');
         process.exit(1);
     }
 };
